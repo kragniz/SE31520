8dfc1d07f3cbc551d2ef5c0add273f0a60abaa13
Louis Taylor <louis@kragniz.eu>
2015-12-03 19:12:04 +0000

    Add wine list thing

--- wine-application/db/seeds.rb
+++ wine-application/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Wine.find_or_create_by(name: 'Test Wine',
+                       image:'http://example.com/3434.jpg',
+                       long_description: 'A longish description',
+                       country: 'England',
+                       grape: 'Big grape',
+                       vegetarian: false,
+                       size: 70,
+                       price: 15.50,
+                       short_description: 'Nice wine'
+                      )
