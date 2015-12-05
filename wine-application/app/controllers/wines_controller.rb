1935c9017e06fcf370875dc6cfe4e250134de0a0
Louis Taylor <louis@kragniz.eu>
2015-12-05 21:30:40 +0000

    Remove unused wine handlers

--- wine-application/app/controllers/wines_controller.rb
+++ wine-application/app/controllers/wines_controller.rb
@@ -13,55 +13,6 @@ class WinesController < ApplicationController
   def show
   end
 
-  # GET /wines/new
-  def new
-    @wine = Wine.new
-  end
-
-  # GET /wines/1/edit
-  def edit
-  end
-
-  # POST /wines
-  # POST /wines.json
-  def create
-    @wine = Wine.new(wine_params)
-
-    respond_to do |format|
-      if @wine.save
-        format.html { redirect_to @wine, notice: 'Wine was successfully created.' }
-        format.json { render :show, status: :created, location: @wine }
-      else
-        format.html { render :new }
-        format.json { render json: @wine.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /wines/1
-  # PATCH/PUT /wines/1.json
-  def update
-    respond_to do |format|
-      if @wine.update(wine_params)
-        format.html { redirect_to @wine, notice: 'Wine was successfully updated.' }
-        format.json { render :show, status: :ok, location: @wine }
-      else
-        format.html { render :edit }
-        format.json { render json: @wine.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /wines/1
-  # DELETE /wines/1.json
-  def destroy
-    @wine.destroy
-    respond_to do |format|
-      format.html { redirect_to wines_url, notice: 'Wine was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_wine
